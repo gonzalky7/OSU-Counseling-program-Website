66a80779efc29a891479d60c29cb38ebaa023e9e
Tim Leavey <timothy.leavey@gmail.com>
2016-10-19 11:15:13 -0700

    Updated php fields

--- form.php
+++ form.php
@@ -10,16 +10,13 @@
 	<h2>Zip: <?php echo $_POST["zip"]; ?> </h1>
 	<h2>Phone: <?php echo $_POST["phone"]; ?> </h1>
 	<h2>Message: <?php echo $_POST["message"]; ?> </h1>
-	<h2>Zip: <?php echo $_POST["zip"]; ?> </h1>
-	<h2>Zip: <?php echo $_POST["zip"]; ?> </h1>
-	<h2>Zip: <?php echo $_POST["zip"]; ?> </h1>
-	<h2>Zip: <?php echo $_POST["zip"]; ?> </h1>
-	<h2>Zip: <?php echo $_POST["zip"]; ?> </h1>
-	<h2>Zip: <?php echo $_POST["zip"]; ?> </h1>
-	<h2>Zip: <?php echo $_POST["zip"]; ?> </h1>
-	<h2>Zip: <?php echo $_POST["zip"]; ?> </h1>
-	<h2>Zip: <?php echo $_POST["zip"]; ?> </h1>
-	<h2>Zip: <?php echo $_POST["zip"]; ?> </h1>
+	<h2>Email: <?php echo $_POST["email"]; ?> </h1>
+	<h2>Occupation: <?php echo $_POST["occupation"]; ?> </h1>
+	<h2>Employer: <?php echo $_POST["employer"]; ?> </h1>
+	<h2>Employer's Address: <?php echo $_POST["workAddress"]; ?> </h1>
+	<h2>Work Phone: <?php echo $_POST["workPhone"]; ?> </h1>
+	<h2>Message: <?php echo $_POST["workMessage"]; ?> </h1>
+
 
 
 
