ba515eaaf18f0ff495f84e1c45b6aa4df322a5d1
Tim Leavey <timothy.leavey@gmail.com>
2016-10-19 11:20:57 -0700

    changed h2 to p tag on form

--- form.php
+++ form.php
@@ -1,21 +1,21 @@
 <html>
-	<h2>Today's date: <?php echo $_POST["date"]; ?> </h1>
-	<h2>First Name: <?php echo $_POST["firstName"]; ?> </h1>
-	<h2>Last Name: <?php echo $_POST["lastName"]; ?> </h1>
-	<h2>Age: <?php echo $_POST["age"]; ?> </h1>
-	<h2>Date of Birth: <?php echo $_POST["Bday"]; ?> </h1>
-	<h2>Address: <?php echo $_POST["address"]; ?> </h1>
-	<h2>City: <?php echo $_POST["city"]; ?> </h1>
-	<h2>State: <?php echo $_POST["state"]; ?> </h1>
-	<h2>Zip: <?php echo $_POST["zip"]; ?> </h1>
-	<h2>Phone: <?php echo $_POST["phone"]; ?> </h1>
-	<h2>Message: <?php echo $_POST["message"]; ?> </h1>
-	<h2>Email: <?php echo $_POST["email"]; ?> </h1>
-	<h2>Occupation: <?php echo $_POST["occupation"]; ?> </h1>
-	<h2>Employer: <?php echo $_POST["employer"]; ?> </h1>
-	<h2>Employer's Address: <?php echo $_POST["workAddress"]; ?> </h1>
-	<h2>Work Phone: <?php echo $_POST["workPhone"]; ?> </h1>
-	<h2>Message: <?php echo $_POST["workMessage"]; ?> </h1>
+	<p>Today's date: <?php echo $_POST["date"]; ?> </p>
+	<p>First Name: <?php echo $_POST["firstName"]; ?> </p>
+	<p>Last Name: <?php echo $_POST["lastName"]; ?> </p>
+	<p>Age: <?php echo $_POST["age"]; ?> </p>
+	<p>Date of Birth: <?php echo $_POST["Bday"]; ?> </p>
+	<p>Address: <?php echo $_POST["address"]; ?> </p>
+	<p>City: <?php echo $_POST["city"]; ?> </p>
+	<p>State: <?php echo $_POST["state"]; ?> </p>
+	<p>Zip: <?php echo $_POST["zip"]; ?> </p>
+	<p>Phone: <?php echo $_POST["phone"]; ?> </p>
+	<p>Message: <?php echo $_POST["message"]; ?> </p>
+	<p>Email: <?php echo $_POST["email"]; ?> </p>
+	<p>Occupation: <?php echo $_POST["occupation"]; ?> </p>
+	<p>Employer: <?php echo $_POST["employer"]; ?> </p>
+	<p>Employer's Address: <?php echo $_POST["workAddress"]; ?> </p>
+	<p>Work Phone: <?php echo $_POST["workPhone"]; ?> </p>
+	<p>Message: <?php echo $_POST["workMessage"]; ?> </p>
 
 
 
