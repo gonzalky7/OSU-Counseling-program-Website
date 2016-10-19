861f2ac38e71181b90bdcd6de672956f50edc0e1
Tim Leavey <timothy.leavey@gmail.com>
2016-10-19 11:26:40 -0700

    added strong tag

--- form.php
+++ form.php
@@ -1,21 +1,21 @@
 <html>
-	<p>Today's date: <?php echo $_POST["date"]; ?> </p>
-	<p>First Name: <?php echo $_POST["firstName"]; ?> </p>
-	<p>Last Name: <?php echo $_POST["lastName"]; ?> </p>
-	<p>Age: <?php echo $_POST["age"]; ?> </p>
-	<p>Date of Birth: <?php echo $_POST["Bday"]; ?> </p>
-	<p>Address: <?php echo $_POST["address"]; ?> </p>
-	<p>City: <?php echo $_POST["city"]; ?> </p>
-	<p>State: <?php echo $_POST["state"]; ?> </p>
-	<p>Zip: <?php echo $_POST["zip"]; ?> </p>
-	<p>Phone: <?php echo $_POST["phone"]; ?> </p>
-	<p>Message: <?php echo $_POST["message"]; ?> </p>
-	<p>Email: <?php echo $_POST["email"]; ?> </p>
-	<p>Occupation: <?php echo $_POST["occupation"]; ?> </p>
-	<p>Employer: <?php echo $_POST["employer"]; ?> </p>
-	<p>Employer's Address: <?php echo $_POST["workAddress"]; ?> </p>
-	<p>Work Phone: <?php echo $_POST["workPhone"]; ?> </p>
-	<p>Message: <?php echo $_POST["workMessage"]; ?> </p>
+	<p><strong>Today's date:</strong> <?php echo $_POST["date"]; ?> </p>
+	<p><strong>First Name:</strong> <?php echo $_POST["firstName"]; ?> </p>
+	<p><strong>Last Name:</strong> <?php echo $_POST["lastName"]; ?> </p>
+	<p><strong>Age:</strong> <?php echo $_POST["age"]; ?> </p>
+	<p><strong>Date of Birth:</strong> <?php echo $_POST["Bday"]; ?> </p>
+	<p><strong>Address:</strong> <?php echo $_POST["address"]; ?> </p>
+	<p><strong>City:</strong> <?php echo $_POST["city"]; ?> </p>
+	<p><strong>State:</strong> <?php echo $_POST["state"]; ?> </p>
+	<p><strong>Zip:</strong> <?php echo $_POST["zip"]; ?> </p>
+	<p><strong>Phone:</strong> <?php echo $_POST["phone"]; ?> </p>
+	<p><strong>Message:</strong> <?php echo $_POST["message"]; ?> </p>
+	<p><strong>Email:</strong> <?php echo $_POST["email"]; ?> </p>
+	<p><strong>Occupation:</strong> <?php echo $_POST["occupation"]; ?> </p>
+	<p><strong>Employer:</strong> <?php echo $_POST["employer"]; ?> </p>
+	<p><strong>Employer's Address:</strong> <?php echo $_POST["workAddress"]; ?> </p>
+	<p><strong>Work Phone:</strong> <?php echo $_POST["workPhone"]; ?> </p>
+	<p><strong>Message:</strong> <?php echo $_POST["workMessage"]; ?> </p>
 
 
 
