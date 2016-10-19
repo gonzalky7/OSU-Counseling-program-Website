170deff8424888c31dcf1715c117fb4a66d47a1a
Tim Leavey <timothy.leavey@gmail.com>
2016-10-19 11:05:52 -0700

    Filled out some of the php form fields.

--- form.php
+++ form.php
@@ -1,3 +1,26 @@
 <html>
-	<h1>You entered: <?php echo $_POST["date"]; ?> </h1>
+	<h2>Today's date: <?php echo $_POST["date"]; ?> </h1>
+	<h2>First Name: <?php echo $_POST["firstName"]; ?> </h1>
+	<h2>Last Name: <?php echo $_POST["lastName"]; ?> </h1>
+	<h2>Age: <?php echo $_POST["age"]; ?> </h1>
+	<h2>Date of Birth: <?php echo $_POST["Bday"]; ?> </h1>
+	<h2>Address: <?php echo $_POST["address"]; ?> </h1>
+	<h2>City: <?php echo $_POST["city"]; ?> </h1>
+	<h2>State: <?php echo $_POST["state"]; ?> </h1>
+	<h2>Zip: <?php echo $_POST["zip"]; ?> </h1>
+	<h2>Phone: <?php echo $_POST["phone"]; ?> </h1>
+	<h2>Message: <?php echo $_POST["message"]; ?> </h1>
+	<h2>Zip: <?php echo $_POST["zip"]; ?> </h1>
+	<h2>Zip: <?php echo $_POST["zip"]; ?> </h1>
+	<h2>Zip: <?php echo $_POST["zip"]; ?> </h1>
+	<h2>Zip: <?php echo $_POST["zip"]; ?> </h1>
+	<h2>Zip: <?php echo $_POST["zip"]; ?> </h1>
+	<h2>Zip: <?php echo $_POST["zip"]; ?> </h1>
+	<h2>Zip: <?php echo $_POST["zip"]; ?> </h1>
+	<h2>Zip: <?php echo $_POST["zip"]; ?> </h1>
+	<h2>Zip: <?php echo $_POST["zip"]; ?> </h1>
+	<h2>Zip: <?php echo $_POST["zip"]; ?> </h1>
+
+
+
 </html>
