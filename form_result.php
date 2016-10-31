3eb882bd87ece54e8607a1745379f2057ed851b6
gonzalky7 <gonzalky@oregonstate.edu>
2016-10-31 09:53:39 -0700

    Added thankyou message and fixed header footer

--- form_result.php
+++ form_result.php
@@ -1,13 +1,13 @@
-<? php
+<?php
 	$db = new mysqli("localhost", "root", "root", "cascades_clinic");
 
 	$sql = "INSERT INTO `patients` (`ID`,`first_name`, `last_name`, `age`, `date_of_birth`) VALUES ( NULL,'$_POST[firstName]',
 	 '$_POST[lastName]', '$_POST[age]', '$_POST[birthday]')";
 
 	if(mysqli_query($db, $sql)){
-    	echo "Thank you Record added successfully.";
+    	echo "Thank you. Record added successfully.";
 	} else{
-    	echo "ERROR: Could not able to execute $sql. " . mysqli_error($db);
+    	echo "ERROR: Could not execute $sql. " . mysqli_error($db);
 	}
 
  
@@ -16,7 +16,12 @@
 ?>
 
 <? include('header.php') ?>
+		<a href="/" class="tag">
+			<div class="banner">
+				<img src="http://osucascades.edu/sites/osucascades.edu/modules/osu_cascades_top_hat/images/osu_cascades_logo.png" class="logo" alt="Oregon State University Cascades">
+			</div>
+		</a>
 	<body>
 	<h1>Thank you...</h1>
 	</body>
-<? include('footer.php') ?>
+<? include('footer.php') ?> 
