e44c7eebc86c9f67f36d5d773627dac8e4106156
JWTappert <tappertj@oregonstate.edu>
2016-10-31 11:39:35 -0700

    Refactored to include db_connect.php

--- form_result.php
+++ form_result.php
@@ -1,5 +1,6 @@
+<? include('db_connect.php') ?>
 <?php
-	$db = new mysqli("localhost", "root", "root", "cascades_clinic");
+	$db = new mysqli("$host", "$username", "$password", "$database");
 
 	$sql = "INSERT INTO `patients` (`ID`,`first_name`, `last_name`, `age`, `date_of_birth`) VALUES ( NULL,'$_POST[firstName]',
 	 '$_POST[lastName]', '$_POST[age]', '$_POST[birthday]')";
