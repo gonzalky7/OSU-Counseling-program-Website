29a35a5df830961eecfb71cf507ad1b7e2f34fdf
JWTappert <tappertj@oregonstate.edu>
2016-11-02 08:51:31 -0700

    Made the background image cover entire background. Eliminated the whaite bars. Fixes #18

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 
 	<body>
 		<div class="banner">
-			<img src="http://osucascades.edu/sites/osucascades.edu/modules/osu_cascades_top_hat/images/osu_cascades_logo.png" class="logo" alt="Oregon State University Cascades">
+			<a href="/index.php"><img src="http://osucascades.edu/sites/osucascades.edu/modules/osu_cascades_top_hat/images/osu_cascades_logo.png" class="logo" alt="Oregon State University Cascades"></a>
 
 			<div class="topright"><a href="#">log in</a></div>
 		</div>
