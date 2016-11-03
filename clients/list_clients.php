bc01c83ad99299db90a8d2947e652eb9313a3c03
gonzalky7 <gonzalky@oregonstate.edu>
2016-11-03 12:19:00 -0700

    list_clients.php: list clients now. Also link for clients in menu bar works on my end. Fixes #49.

--- clients/list_clients.php
+++ clients/list_clients.php
@@ -1,8 +1,31 @@
-<?php include("..includes/header.php") ?>
+<?php include("../includes/header.php") ?>
+
+<?php include("../db_connect.php") ?>
+
+   <div class="navBar">
+       		<ul>
+  			<li><a class="active" href="../index.php">Home</a></li>
+  			<li><a href="../clients/list_clients.php">Clients</a></li>
+  			<li><a href="#Users">Users</a></li>
+  			<li><a href="#about">About</a></li>
+			</ul>
+  </div>	
+
+<?php 
+
+$res = $db->query("SELECT ID, first_name FROM patients"); ?>
+<ol>
+<?php while($row = $res->fetch_assoc()) { ?> 
+	 	<li> First name: <a href="view_patient_info.php?ID= <?php echo $row['ID']; ?>"> <?php echo $row['first_name']; ?></a>
+ 	<a href="delete.php?ID=<?php echo $row['ID']; ?>">Delete</a> </li><br>
+
+<?php } ?> </ol>
+
+<?php $db->close(); ?>
 
 <?php
-	include("../db_connect.php");
-	
+/*
+ 
 	#Create an array of all entries in the db
 	$get = "SELECT * FROM patients ORDER BY id";
 	$resultArray = mysqli_query($db, $get);
@@ -28,7 +51,7 @@
 		#print an error because nothing was found
 		echo "Error: No entries found";
 	}
-
+*/
 ?>
 
-<? include("footer.php") ?> 
+<?php include("../includes/footer.php") ?> 
