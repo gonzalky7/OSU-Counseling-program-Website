c2c9717186509a976862be01b161fa29176f9c87
Yong Bakos <ybakos@humanoriented.com>
2016-11-04 16:10:20 -0700

    Fix css paths and other urls to root-relative. Add Roles link to menu bar. Fixes #75.

--- Includes/header.php
+++ Includes/header.php
@@ -2,9 +2,9 @@
 <html lang="en">
 	<head>
 		<title>OSU-Cascades Counseling Practicum</title>
-		<link rel="stylesheet" type="text/css" href="../CSS/style.css">
-		<link rel="stylesheet" type="text/css" href="../CSS/form.css">
-		<link rel="stylesheet" type="text/css" href="../CSS/login.css">
+		<link rel="stylesheet" type="text/css" href="/css/style.css">
+		<link rel="stylesheet" type="text/css" href="/css/form.css">
+		<link rel="stylesheet" type="text/css" href="/css/login.css">
 		<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,700">
 	</head>
 
@@ -16,10 +16,11 @@
 
 		<div class="navBar">
        		<ul>
-  			<li><a class="active" href="../index.php">Home</a></li>
-  			<li><a href="../clients/list_clients.php">Clients</a></li>
-  			<li><a href="#Users">Users</a></li>
-  			<li><a href="#about">About</a></li>
+  			<li><a class="active" href="/index.php">Home</a></li>
+  			<li><a href="/clients/list_clients.php">Clients</a></li>
+  			<li><a href="/users">Users</a></li>
+        <li><a href="/roles">Roles</a></li>
+  			<li><a href="/about.php">About</a></li>
 			</ul>
   		</div>
 
