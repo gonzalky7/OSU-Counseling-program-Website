4542eaf3e24ca2183722719e84ef5e55d9184d4c
gonzalky7 <gonzalky@oregonstate.edu>
2016-11-04 11:15:36 -0700

    Fixed app ability to delete clients. Fixes #51.

--- Clients/delete.php
+++ Clients/delete.php
@@ -1,7 +1,21 @@
+<?php include("../includes/header.php") ?>
+
+<?php include("../db_connect.php") ?>
+
 <?php 
 
-include('../db_connect.php');
+$id = $_GET["ID"];
 
+$res = $db->query("DELETE FROM 'patients' WHERE ID=$id");
 
+if( $db->query($res) === TRUE){
+	echo "Recorded successfully deleted"; 
+}else {
+	echo "Error deleting record: " . $db->error;
+}
+
+$db->close();
 
 ?>
+
+<?php include("../includes/footer.php") ?> 
