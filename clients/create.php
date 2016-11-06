eb344a2a39c7ba9a589f8f0d2ea6a88154b18665
JWTappert <tappertj@oregonstate.edu>
2016-11-06 13:36:07 -0800

    Was working on redirects but got nothing working

--- clients/create.php
+++ clients/create.php
@@ -2,22 +2,26 @@
 <?php include("../includes/header.php") ?>
 
 <?php
+	ob_start();
 	include("../db_connect.php");
 	//For the db.humanoriented.com the $dbname is db_oneteam not cascades_clinic
 	//$db = new mysqli($server, $username, $password, $dbname);
 
-
-
-
 	$sql = "INSERT INTO clients (id, first_name, last_name, age, date_of_birth) VALUES ( NULL,'$_POST[firstName]',
 	 '$_POST[lastName]', '$_POST[age]', '$_POST[birthday]')";
 
+	echo "<div class=\"wrapper\">";
+	echo "    <div class=\"main-content\">";
+
 	if(mysqli_query($db, $sql)){
-    	echo "<h1>Thank you</h1>";
-    	echo "<p>Your record was successfuly added.</p>";
+		echo "<h1>Thank you</h1>";
+    	echo "<p style=\"text-align: center\">Your record was successfuly added.</p>";
 	} else{
     	echo "ERROR: Could not execute $sql. " . mysqli_error($db);
 	}
+
+	echo "    </div>";
+	echo "</div>";
 
 	// Close connection
 	mysqli_close($db);
