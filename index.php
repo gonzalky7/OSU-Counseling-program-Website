af3a1c4e037e3c2a12b1aa058651d0e1e1bfd22c
JWTappert <tappertj@oregonstate.edu>
2016-11-06 20:19:49 -0800

    Implemented basic session tracking. Still a work in progress but a user that provides correct username and password will start a new session. Fixes #82

--- index.php
+++ index.php
@@ -1,4 +1,17 @@
-<?php include('includes/header.php') ?>
+<?php 
+	ob_start();
+	session_start();
+
+	include('includes/header.php');
+
+
+	if (isset($_SESSION['user_id']) && !empty($_SESSION['user_id'])){
+			echo "You're logged in.";
+		}else{
+			echo "Not logged in." . $_SESSION['user_id'];
+	}
+?>
+
 <div class="wrapper">	
         <div class="mainBox">
        		<div class="welcome-header">
--- session_new.php
+++ session_new.php
@@ -1,37 +1,34 @@
-
-<?php include("db_connect.php") ?>
-
 <?php 
+	ob_start(); 
+	session_start();
+ 
+ 	include("db_connect.php");
+	
+	if (isset($_POST['username']) && isset($_POST['password'])){
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+	}//TODO: else post message "Please Enter your username and password"
 
-	if (isset($_POST['username'])&&isset($_POST['password'])){
-				$username = $_POST['username'];
-				$password = $_POST['password'];
-	}
+	if (!empty($username) && !empty($password)) {
+		$query = "SELECT id FROM users WHERE username = '$username' AND password = '$password'"; 
 
-	if (!empty($username)&&!empty($password)){
-				echo 'Username password entered.';
-	} else {
-		echo 'Error no username or password entered ';
-	}
+		//echo $username;
+		$res = $db->query($query);
+		$row = $res->fetch_assoc();
 
-
-if (!empty($username)&& !empty($password)) {
-	$query = "SELECT id FROM users WHERE username = '$username' AND password = '$password'"; 
-
-	//echo $username;
-	$res = $db->query($query);
-
-	if ($res) {
-		$row_cnt = $res->num_rows; 
+		if ($res) {
+			$row_cnt = $res->num_rows; 
 		
-		if ($row_cnt == 0 ) {
-			echo 'Invalid username/password combinations. '; 
-		} else if ($row_cnt == 1){
-			echo $username;
+			if ($row_cnt == 0 ) {
+				echo 'Invalid username/password combination.'; 
+			} else if ($row_cnt == 1){
+				$user_id = $row['id'];
+				$_SESSION['user_id'] = $user_id;
+				header('Location: index.php');
+			}
+		
 		}
-		
 	}
-}
 
 
 ?>
