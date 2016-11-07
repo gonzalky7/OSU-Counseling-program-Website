018d22f51982cc47ae52b51b6e42d2e3d2f0c17f
JWTappert <tappertj@oregonstate.edu>
2016-11-06 20:55:23 -0800

    Started on a user class

---
+++ classes/user.class.php
@@ -0,0 +1,21 @@
+<?php
+	/*The user class represents a user of our system. The user class should represent the user's important data as well as the actions a user should be able to take.*/
+	include("../db_connect.php");
+
+	class User {
+		public $id;
+		public $first_name;
+		public $last_name;
+		public $username;
+		public $password;
+		public $role_id;
+
+		//What kinds of things will users be able to do...?
+
+
+
+	}
+
+
+
+?>
