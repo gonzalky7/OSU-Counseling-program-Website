9a7287e6712432a0d85573d90e107ac395011426
gonzalky7 <gonzalky@oregonstate.edu>
2016-11-13 20:39:12 -0800

    Fixed index.php table

--- roles/index.php
+++ roles/index.php
@@ -2,7 +2,7 @@
 	include('../includes/header.php');
 	include('../classes/role.class.php');
 
-	$list_roles = new Role();
+	$list_roles = new Role(NULL,NULL);
 	$roles = $list_roles->load();
 ?>
 
@@ -17,10 +17,20 @@
            </tr>
            <?php foreach ($roles as $value){
            			echo "<tr>";
-           			echo "<td>" . $value->name . "</td>";
-           			echo "<td>" . $value->id . "</td>";
-           			echo "</tr>";
-                 }
+                echo "<td>";
+                echo $value->name;
+                echo "</td>";
+                echo "<td>";
+                echo $value->id;
+                echo "</td>";
+                echo "<td>";
+                echo "<a style=\"color: red; text-decoration: none; text-align: center;\" href=\"delete.php?id=" . $value->id . "\">X</a>";
+                echo "</td>";
+                echo "<td>";
+                echo "<a style=\"color: Green; text-decoration: none; text-align: center;\" href=\"update.php?id=" . $value->id . "\">&#10003</a>";
+                echo "</td>";
+               echo "</tr>";
+             }
             ?>
             <tr>
               <th><a href="form.php">Create New Role</a></th>
