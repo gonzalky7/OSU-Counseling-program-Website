31056b215da8d7e57b5670d012a522e9c36ba473
gonzalky7 <gonzalky@oregonstate.edu>
2016-11-29 18:52:00 -0800

    Added documentation to session_functions. References #139.

--- includes/session.functions.php
+++ includes/session.functions.php
@@ -4,13 +4,26 @@
 */
 	include("db_connect.php");
 
-	// Returns a User object or NULL if no user is logged in.
+/*This function checks to see if any user is logged in at the moment.
+ *
+ *This function takes no parameters.
+ *
+ *This function returns true if a session is set, or returns false if there is no user logged in.
+ */
 	function currentUser() {
   		if (isset($_SESSION["user_id"])) {
   			return true;
   		} else return false;
  	}
 
+	/*This function checks to see if a user is logged and if not it will redirect the user to the login page 
+	 *if trying to access web pages that requires user login. This function is dependent on predetermined
+	 *public_urls that are accessible for public users.
+	 *
+ 	 *This function takes no parameters.
+ 	 *
+ 	 *This function does not return anything.
+ 	 */
   	function redirectIfNotLoggedIn() {
       include("public_urls.include.php");
 
@@ -21,14 +34,25 @@
           die();
       }
   	}
-
+    /*This function checks if there are any current sessions, or any users logged in.
+	 *
+ 	 *This function takes no parameters.
+ 	 *
+ 	 *This function does not return the session username
+ 	 */
   	function currentUserName(){
     	if (isset($_SESSION["user_name"])) {
   	 		return $_SESSION["user_name"];
     	} else return NULL;
  	 }
 
-  	//return $_SESSION['currentUser'];
+  	/*This function checks to see if username and password entered in the login page match the database user
+  	 *name and password in the database to verify user credentials.
+	 *
+ 	 *This functions takes username and password for parameters
+ 	 *
+ 	 *This function does not returns true if credentials are valid or returns false if they do not. 
+ 	 */
   	function validateUser($username, $password) {
         global $db;
 
@@ -36,28 +60,31 @@
 
     	  $result = $db->query($query);
 
-    	  if ($result && $result->num_rows == 1) {
+    	if ($result && $result->num_rows == 1) {
       	    $row = $result->fetch_assoc();
       	    $user_id = $row["id"];
             $pass_hash = $row["password"];
 
-            if(password_verify("$password", "$pass_hash")){
+            if(password_verify("$password", "$pass_hash")) {
       	        //Declaring global session variables
       	        $_SESSION["user_id"] = $user_id;
       	        $_SESSION["user_name"] = $username;
                 return true;
-            } else {
-              $_SESSION["message"] = "Invalid username/password";
-              return false;
-            }
-    	  } else {
-      		  $_SESSION["message"] = "Invalid username/password";
-      		  return false;
-    		}
+            } 	else {
+              		$_SESSION["message"] = "Invalid username/password";
+              		return false;
+          	  	}
+    	} 
 
     }
-
-
+    /*This function checks if "message" variable has been set in the session. THis function is dependent
+     *on the validateUser() function which will set "message" if credentials are incorrect. The message will
+     *echo on the login form "Invalid username/password"
+	 *
+ 	 *This function takes no parameters.
+ 	 *
+ 	 *This function does not return anything.
+ 	 */
     function errorMessageLogin(){
        if (isset($_SESSION["message"])) {
           echo $_SESSION["message"];
